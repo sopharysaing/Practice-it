8bef51f71f7c3fee246d9d14886b5fe3729286b8
ssphary45 <kentichu@gmail.com>
2018-12-19 17:19:10 +0700

    start up

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+    public static void main(String[] args) {
+
+        //int[3][4] a = new int{{3, 4, 5, 6}, {4, 5, 6, 7}, {5, 6, 7, 8}};
+        //mystery2d(a);{
+        int[][] a = new int[3][4];
+        for (int r = 0; r < a.length; r++) {
+            for (int c = 0; c < a[r].length; c++) {
+
+            }
+        }
+    }
+}
